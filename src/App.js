d75cf4f42d28165d48e3bc6dc4e156fb320083dd
Vertical <ambintasso@gmail.com>
2023-01-24 13:51:18 +0530

    first import

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import General from "./components/general";
+import Education from "./components/educational";
+import Practical from "./components/practical";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <General />
+      <Education />
+      <Practical />
     </div>
   );
 }
