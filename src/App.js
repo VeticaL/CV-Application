f7723ffae1cc88a0b469eb32772e4e8e99a5981a
Vertical <ambintasso@gmail.com>
2023-01-26 11:57:46 +0530

    structturing

--- src/App.js
+++ src/App.js
@@ -6,9 +6,19 @@ import Practical from "./components/practical";
 function App() {
   return (
     <div className="App">
-      <General />
-      <Education />
-      <Practical />
+      <h2>CV CREATOR</h2>
+      <div className="general">
+        <h3>Personal Information</h3>
+        <General />
+      </div>
+      <div className="general">
+      <h3>Education</h3>
+        <Education />
+      </div>
+      <div className="general">
+      <h3>Experience</h3>
+        <Practical />
+      </div>
     </div>
   );
 }
