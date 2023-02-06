aa9c43976fbebb93ee38455de8dd3bf0df38751a
Vertical <ambintasso@gmail.com>
2023-02-06 12:48:56 +0530

    class

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import "./App.css";
+import React from "react";
 import General from "./components/general";
 import Education from "./components/educational";
 import Practical from "./components/practical";
 
-function App() {
-  return (
-    <div className="App">
-      <h2>CV CREATOR</h2>
-      <div className="general">
-        <h3>Personal Information</h3>
-        <General />
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>CV CREATOR</h2>
+        <div className="general">
+          <h3>Personal Information</h3>
+          <General />
+        </div>
+        <div className="general">
+        <h3>Education</h3>
+          <Education />
+        </div>
+        <div className="general">
+        <h3>Experience</h3>
+          <Practical />
+        </div>
       </div>
-      <div className="general">
-      <h3>Education</h3>
-        <Education />
-      </div>
-      <div className="general">
-      <h3>Experience</h3>
-        <Practical />
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
