5cf6fe893ed49f74c393408346a4679af2c47564
Vertical <ambintasso@gmail.com>
2023-03-10 13:34:45 +0530

    format

--- src/components/general.js
+++ src/components/general.js
@@ -10,11 +10,10 @@ class General extends Component {
       address: "",
     };
     this.updateInput = this.updateInput.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   updateInput(event) {
-    if(event.target.className === "name") {
+    if (event.target.className === "name") {
       this.setState({ Name: event.target.value });
     } else if (event.target.className === "email") {
       this.setState({ email: event.target.value });
@@ -25,23 +24,24 @@ class General extends Component {
     }
   }
 
-  handleSubmit(e) {
-    e.preventDefault()
-    console.log("Your input value is: " + this.state.Name);
-    console.log("email: " + this.state.email);
-    console.log("phonenumber: " + this.state.phonenumber);
-    console.log("address: " + this.state.address);
-    //Send state to the server code
-  }
-
   render() {
     return (
       <div>
         <form>
           <label htmlFor="name"></label>
-          <input type="text" className="name" placeholder="Full Name" onChange={this.updateInput}></input>
+          <input
+            type="text"
+            className="name"
+            placeholder="Full Name"
+            onChange={this.updateInput}
+          ></input>
           <label htmlFor="email"></label>
-          <input type="email" className="email" placeholder="Email" onChange={this.updateInput}></input>
+          <input
+            type="email"
+            className="email"
+            placeholder="Email"
+            onChange={this.updateInput}
+          ></input>
           <label htmlFor="phoneNumber"></label>
           <input
             type="tel"
@@ -50,10 +50,12 @@ class General extends Component {
             onChange={this.updateInput}
           ></input>
           <label htmlFor="Address"></label>
-          <input type="text" className="address" placeholder="Address" onChange={this.updateInput}></input>
-          <button type="submit" onClick={this.handleSubmit}>
-            Submit
-          </button>
+          <input
+            type="text"
+            className="address"
+            placeholder="Address"
+            onChange={this.updateInput}
+          ></input>
         </form>
         <p>Name : {this.state.Name}</p>
         <p>Email : {this.state.email}</p>
