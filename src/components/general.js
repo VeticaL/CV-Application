84f5680fb4a061f720e70585b7e94309ebb9b199
Vertical <ambintasso@gmail.com>
2023-03-10 12:42:12 +0530

    preview

--- src/components/general.js
+++ src/components/general.js
@@ -9,37 +9,56 @@ class General extends Component {
       phonenumber: "",
       address: "",
     };
+    this.updateInput = this.updateInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  preview = () => {
-    this.setState({
-      Name: document.getElementsByClassName("name").value,
-      email: document.getElementsByClassName("email").value,
-      phonenumber: document.getElementsByClassName("phonenumber").value,
-      address: document.getElementsByClassName("address").value,
-    });
-  };
+  updateInput(event) {
+    if(event.target.className === "name") {
+      this.setState({ Name: event.target.value });
+    } else if (event.target.className === "email") {
+      this.setState({ email: event.target.value });
+    } else if (event.target.className === "phonenumber") {
+      this.setState({ phonenumber: event.target.value });
+    } else if (event.target.className === "address") {
+      this.setState({ address: event.target.value });
+    }
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+    console.log("Your input value is: " + this.state.Name);
+    console.log("email: " + this.state.email);
+    console.log("phonenumber: " + this.state.phonenumber);
+    console.log("address: " + this.state.address);
+    //Send state to the server code
+  }
 
   render() {
     return (
       <div>
         <form>
           <label htmlFor="name"></label>
-          <input type="text" className="name" placeholder="Full Name"></input>
+          <input type="text" className="name" placeholder="Full Name" onChange={this.updateInput}></input>
           <label htmlFor="email"></label>
-          <input type="email" className="email" placeholder="Email"></input>
+          <input type="email" className="email" placeholder="Email" onChange={this.updateInput}></input>
           <label htmlFor="phoneNumber"></label>
           <input
             type="tel"
             className="phonenumber"
             placeholder="Phone Number"
+            onChange={this.updateInput}
           ></input>
           <label htmlFor="Address"></label>
-          <input type="text" className="address" placeholder="Address"></input>
-          <button type="submit" onClick={this.preview}>
+          <input type="text" className="address" placeholder="Address" onChange={this.updateInput}></input>
+          <button type="submit" onClick={this.handleSubmit}>
             Submit
           </button>
         </form>
+        <p>Name : {this.state.Name}</p>
+        <p>Email : {this.state.email}</p>
+        <p>Phone : {this.state.phonenumber}</p>
+        <p>Address : {this.state.address}</p>
       </div>
     );
   }
